cabfdb2f1e1b58bf8426ce0eb960a7831f703ecb
Rasmus Schenström <element_004@hotmail.com>
2024-05-07 18:57:29 +0200

    Add missing package
    Tracking-Id: EIP

--- Hnefatafl/argumentParser.hpp
+++ Hnefatafl/argumentParser.hpp
@@ -10,6 +10,7 @@
 
 #include <string>
 #include <vector>
+#include <array>
 
 #include "configurations.hpp"
 
@@ -18,13 +19,13 @@ class ArgumentParser
     private:
         std::vector<std::array<std::string, 4>> arguments;
     
-    ArgumentParser();
-    short matchArguments(std::string argument);
-    
-    void printHelp(const float VERSION);
-    bool setSize(Configurations*& configurations, int i, int argc, const char* argv[]);
-    bool setPort(Configurations*& configurations, int i, int argc, const char* argv[]);
-    std::vector<std::array<std::string, 4>> getArguments();
+        ArgumentParser();
+        short matchArguments(std::string argument);
+        
+        void printHelp(const float VERSION);
+        bool setSize(Configurations*& configurations, int i, int argc, const char* argv[]);
+        bool setPort(Configurations*& configurations, int i, int argc, const char* argv[]);
+        std::vector<std::array<std::string, 4>> getArguments();
         
     public:
         static Configurations* parseArguments(const float VERSION, int argc, const char* argv[]);
